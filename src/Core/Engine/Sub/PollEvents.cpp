beb0a1fc445587b4fe9bae853e26fe1053e5a64d
Andreas Åkerberg <zmallwood@proton.me>
2025-09-27 20:05:55 +0200

    Save

--- src/Core/Engine/Sub/PollEvents.cpp
+++ src/Core/Engine/Sub/PollEvents.cpp
@@ -8,8 +8,8 @@
 #include "Core/Input/Keyboard/KeyboardInput.hpp"
 #include "Core/Input/Mouse/MouseInput.hpp"
 
- namespace Forradia
- {
+namespace Forradia
+{
     void PollEvents()
     {
         SDL_Event event;
@@ -19,41 +19,29 @@
             switch (event.type)
             {
             case SDL_QUIT:
-
                 _<Engine>().Stop();
-
                 break;
 
             case SDL_KEYDOWN:
-
                 _<KeyboardInput>().RegisterKeyPress(
                     event.key.keysym.sym);
-
                 break;
 
             case SDL_KEYUP:
-
                 _<KeyboardInput>().RegisterKeyRelease(
                     event.key.keysym.sym);
-
                 break;
 
             case SDL_MOUSEBUTTONDOWN:
-
                 _<MouseInput>().RegisterMouseButtonDown(
-                    event.button.button
-                );
-
+                    event.button.button);
                 break;
 
             case SDL_MOUSEBUTTONUP:
-
                 _<MouseInput>().RegisterMouseButtonUp(
-                    event.button.button
-                );
-
+                    event.button.button);
                 break;
             }
         }
     }
- }
+}
