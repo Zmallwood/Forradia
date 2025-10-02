9eb254e611904be5967d0ae8bacd477ffdcbce9a
Andreas Åkerberg <zmallwood@proton.me>
2025-10-02 19:39:39 +0200

    Save

--- src/theme_0/mechanics/npcs_update/update_npcs.cpp
+++ src/theme_0/mechanics/npcs_update/update_npcs.cpp
@@ -3,7 +3,55 @@
  * This code is licensed under MIT license (see LICENSE for details)
  */
 #include "update_npcs.hpp"
+#include "theme_0/mechanics/world_structure/npc.hpp"
+#include "theme_0/mechanics/world_structure/tile.hpp"
+#include "theme_0/mechanics/world_structure/world.hpp"
+#include "theme_0/mechanics/world_structure/world_area.hpp"
 
 namespace forr {
-  void update_npcs() {}
+  void update_npcs() {
+    auto world_area{get_singleton<world>().get_current_world_area()};
+    auto &npcs_mirror_ref{world_area->get_npcs_mirror_ref()};
+    auto now{get_ticks()};
+    for (auto it = npcs_mirror_ref.begin(); it != npcs_mirror_ref.end();) {
+      auto npc{it->first};
+      auto position{it->second};
+      if (now < npc->get_ticks_last_move() +
+                    invert_movement_speed(npc->get_movement_speed())) {
+        ++it;
+        continue;
+      }
+      auto destination{npc->get_destination()};
+      if (destination.x == -1 && destination.y == -1) {
+        auto new_destination_x{position.x + random_int(11) - 5};
+        auto new_destination_y{position.y + random_int(11) - 5};
+        npc->set_destination({new_destination_x, new_destination_y});
+      }
+      auto dx{npc->get_destination().x - position.x};
+      auto dy{npc->get_destination().y - position.y};
+      auto norm_dx{normalize(dx)};
+      auto norm_dy{normalize(dy)};
+      auto new_x{position.x + norm_dx};
+      auto new_y{position.y + norm_dy};
+      auto new_position{point{new_x, new_y}};
+      if (new_position == npc->get_destination()) {
+        npc->set_destination({-1, -1});
+      }
+      auto tile{world_area->get_tile(new_position.x, new_position.y)};
+      if (tile && !tile->get_npc() &&
+          tile->get_ground() != hash("GroundWater")) {
+        auto old_position{position};
+        npc->set_ticks_last_move(now);
+        auto old_tile{world_area->get_tile(old_position.x, old_position.y)};
+        auto new_tile{world_area->get_tile(new_position.x, new_position.y)};
+        old_tile->set_npc(nullptr);
+        new_tile->set_npc(npc);
+        npcs_mirror_ref.erase(npc);
+        npcs_mirror_ref.insert({npc, {new_position.x, new_position.y}});
+      } else {
+        npc->set_destination({-1, -1});
+      }
+      ++it;
+    }
+  }
 }
--- src/theme_0/mechanics/world_structure/npc.hpp
+++ src/theme_0/mechanics/world_structure/npc.hpp
@@ -11,6 +11,16 @@ namespace forr {
 
     auto get_type() const { return m_type; }
 
+    auto get_ticks_last_move() const { return m_ticks_last_move; }
+
+    void set_ticks_last_move(int value) { m_ticks_last_move = value; }
+
+    auto get_movement_speed() const { return m_movement_speed; }
+
+    auto get_destination() const { return m_destination; }
+
+    void set_destination(point value) { m_destination = value; }
+
   private:
     void initialize();
 
@@ -18,5 +28,8 @@ namespace forr {
 
     int m_type{0};
     str m_name;
+    int m_ticks_last_move{0};
+    float m_movement_speed{2.0f};
+    point m_destination{-1, -1};
   };
 }
