bd584f5720879d9860b3d7849da035265ec04b55
Andreas Åkerberg <zmallwood@proton.me>
2025-10-03 15:30:51 +0200

    Save

--- src/theme_0/func/creas_update/update_crea_movem.cpp
+++ src/theme_0/func/creas_update/update_crea_movem.cpp
@@ -3,9 +3,6 @@
  * This code is licensed under MIT license (see LICENSE for details)
  */
 #include "update_crea_movem.hpp"
-#include "sub/calc_new_crea_pos.hpp"
-#include "sub/gen_new_crea_dest.hpp"
-#include "sub/move_crea_to_new_loc.hpp"
 #include "theme_0/func/world_struct/creature.hpp"
 #include "theme_0/func/world_struct/tile.hpp"
 #include "theme_0/func/world_struct/world.hpp"
@@ -24,15 +21,35 @@ namespace forr {
         ++it;
         continue;
       }
-      gen_new_crea_dest(crea);
-      auto new_pos{calc_new_crea_pos(crea)};
+      auto dest{crea->get_dest()};
+      if (dest.x == -1 && dest.y == -1) {
+        auto new_dest{pos.x + rand_int(11) - 5};
+        auto new_destination_y{pos.y + rand_int(11) - 5};
+        crea->set_dest({new_dest, new_destination_y});
+      }
+      auto w_area{_<world>().get_curr_w_area()};
+      auto &creas{w_area->get_creatures_mirror_ref()};
+      auto dx{crea->get_dest().x - pos.x};
+      auto dy{crea->get_dest().y - pos.y};
+      auto norm_dx{normalize(dx)};
+      auto norm_dy{normalize(dy)};
+      auto new_x{pos.x + norm_dx};
+      auto new_y{pos.y + norm_dy};
+      point new_pos{new_x, new_y};
       if (new_pos == crea->get_dest()) {
         crea->set_dest({-1, -1});
       }
       auto tl{w_area->get_tl(new_pos.x, new_pos.y)};
       if (tl && !tl->get_creature() &&
           tl->get_ground() != hash("GroundWater")) {
-        move_crea_to_new_loc(crea, {new_pos.x, new_pos.y});
+        auto old_pos{creas.at(crea)};
+        crea->set_ticks_last_move(now);
+        auto old_tile{w_area->get_tl(old_pos.x, old_pos.y)};
+        auto new_tile{w_area->get_tl(new_pos.x, new_pos.y)};
+        old_tile->set_creature(nullptr);
+        new_tile->set_creature(crea);
+        creas.erase(crea);
+        creas.insert({crea, {new_pos.x, new_pos.y}});
       } else {
         crea->set_dest({-1, -1});
       }
