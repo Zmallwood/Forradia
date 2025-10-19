145e429365ad533ad73907ba080dc06dfe95adc3
Andreas Åkerberg <zmallwood@proton.me>
2025-10-19 20:08:13 +0200

    Save

--- desktop/run.sh
+++ desktop/run.sh
@@ -4,7 +4,9 @@ directory="./bin"
 if [ ! -d "$directory" ]; then
     mkdir -p "$directory"
 fi
+rm -rf ./bin/res
 cp ./build/Forradia ./bin/
 cp -r ./res ./bin/
+cp -r ./src_theme_0_scripts_py/ ./bin/res/theme_0_scripts
 cd ./bin
 ./Forradia
