15080376bc9f9fa7523c75588f6d156f8408be6c
Andreas Åkerberg <zmallwood@proton.me>
2025-11-06 22:48:01 +0100

    Revert "Save"
    This reverts commit f387affa19d807d969ffa54966c25f1668290e67.

--- desktop/code/client/theme_0/src/CustomGUI/GUISystemMenu.cpp
+++ desktop/code/client/theme_0/src/CustomGUI/GUISystemMenu.cpp
@@ -10,29 +10,11 @@
 
 #include "Color2DRenderer.hpp"
 
-#include "GUIButton.hpp"
-
-#include "GameSaveFiles/GameSaving.hpp"
-
-#include "GUIChatBox.hpp"
-
 namespace Forradia::Theme0
 {
     void GUISystemMenu::Initialize()
     {
         this->SetVisible(false);
-
-        auto saveButton{std::make_shared<GUIButton>(
-            "GUIButtonSystemMenuSaveGame",
-            0.5f - 0.08 / 2.0f, 0.5f, 0.08f, 0.05f,
-            "Save game",
-            []
-            {
-                _<GameSaving>().SaveGame();
-                _<GUIChatBox>().Print("Game saved.");
-            })};
-
-        this->AddChildComponent(saveButton);
     }
 
     void GUISystemMenu::UpdateDerived()
@@ -50,5 +32,21 @@ namespace Forradia::Theme0
             k_renderIDBackgroundColor,
             {1.0f, 0.0f, 0.7f, 0.5f}, 0.0f, 0.0f, 1.0f,
             1.0f);
+
+        // auto
+        // c_sz{GetCanvasSize(_<SDLDevice>().GetWindow())};
+
+        // auto rect{SDL_Rect{0, 0, c_sz.w,
+        // c_sz.h}};
+
+        // SDL_SetRenderDrawBlendMode(_<SDLDevice>().rend().get(),
+        //                            SDL_BLENDMODE_BLEND);
+
+        // SDL_SetRenderDrawColor(_<SDLDevice>().rend().get(),
+        // 200, 0,
+        //                        255, 50);
+
+        // SDL_RenderFillRect(_<SDLDevice>().rend().get(),
+        // &rect);
     }
 }
