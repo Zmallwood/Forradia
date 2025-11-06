e8769f10a1ee8211e62a040a62bc98368c9b652a
Andreas Åkerberg <zmallwood@proton.me>
2025-11-06 22:53:11 +0100

    Save

--- desktop/code/client/theme_0/src/CustomGUI/GUISystemMenu.cpp
+++ desktop/code/client/theme_0/src/CustomGUI/GUISystemMenu.cpp
@@ -10,11 +10,22 @@
 
 #include "Color2DRenderer.hpp"
 
+#include "GUIButton.hpp"
+
+#include "GUIChatBox.hpp"
+
 namespace Forradia::Theme0
 {
     void GUISystemMenu::Initialize()
     {
         this->SetVisible(false);
+
+        auto saveGameButton{std::make_shared<GUIButton>(
+            "GUIButtonSaveGame", 0.5f - 0.08f / 2, 0.5f,
+            0.08f, 0.05f, "Save game", [this]
+            { _<GUIChatBox>().Print("Saving game..."); })};
+
+        this->AddChildComponent(saveGameButton);
     }
 
     void GUISystemMenu::UpdateDerived()
@@ -32,21 +43,5 @@ namespace Forradia::Theme0
             k_renderIDBackgroundColor,
             {1.0f, 0.0f, 0.7f, 0.5f}, 0.0f, 0.0f, 1.0f,
             1.0f);
-
-        // auto
-        // c_sz{GetCanvasSize(_<SDLDevice>().GetWindow())};
-
-        // auto rect{SDL_Rect{0, 0, c_sz.w,
-        // c_sz.h}};
-
-        // SDL_SetRenderDrawBlendMode(_<SDLDevice>().rend().get(),
-        //                            SDL_BLENDMODE_BLEND);
-
-        // SDL_SetRenderDrawColor(_<SDLDevice>().rend().get(),
-        // 200, 0,
-        //                        255, 50);
-
-        // SDL_RenderFillRect(_<SDLDevice>().rend().get(),
-        // &rect);
     }
 }
