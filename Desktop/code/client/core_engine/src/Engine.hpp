a0bad24919a1bb37020cb8a46c621c06c1c3753c
Andreas Åkerberg <zmallwood@proton.me>
2025-11-11 20:18:25 +0100

    Save

--- Desktop/code/client/core_engine/src/Engine.hpp
+++ Desktop/code/client/core_engine/src/Engine.hpp
@@ -8,18 +8,37 @@
 
 namespace Forradia
 {
+    ///
+    /// The engine class is responsible for initializing, running and stopping
+    /// the game.
+    ///
     class Engine
     {
       public:
+        ///
+        /// Initializes the engine.
+        ///
+        /// @param gameWindowTitle The title of the game window.
+        /// @param clearColor The color to clear the canvas with.
+        ///
         void Initialize(StringView gameWindowTitle, Color clearColor) const;
 
+        ///
+        /// Runs the engine.
+        ///
         void Run();
 
+        ///
+        /// Stops the engine.
+        ///
         void Stop();
 
       private:
+        ///
+        /// Polls and handles events.
+        ///
         void HandleEvents();
 
-        bool m_running{true};
+        bool m_running{true}; ///< Whether the engine is running.
     };
 }
--- Desktop/code/client/theme_0/src/Main.cpp
+++ Desktop/code/client/theme_0/src/Main.cpp
@@ -17,44 +17,6 @@ int main(int argc, char **argv)
     using namespace Forradia;
     using namespace Theme0;
 
-    // std::cout << "GroundGrass: "
-    //           <<
-    //           static_cast<uint64_t>(Hash("GroundGrass"))
-    //           << std::endl;
-    // std::cout << "GroundRock: "
-    //           <<
-    //           static_cast<uint64_t>(Hash("GroundRock"))
-    //           << std::endl;
-    // std::cout << "GroundDirt: "
-    //           <<
-    //           static_cast<uint64_t>(Hash("GroundDirt"))
-    //           << std::endl;
-    // std::cout << "GroundWater: "
-    //           <<
-    //           static_cast<uint64_t>(Hash("GroundWater"))
-    //           << std::endl;
-    // std::cout << "GroundCobbleStone: "
-    //           <<
-    //           static_cast<uint64_t>(Hash("GroundCobbleStone"))
-    //           << std::endl;
-
-    // std::cout << "ObjectFirTree: "
-    //           << static_cast<uint64_t>(
-    //                  Hash("ObjectFirTree"))
-    //           << std::endl;
-    // std::cout << "ObjectBirchTree: "
-    //           << static_cast<uint64_t>(
-    //                  Hash("ObjectBirchTree"))
-    //           << std::endl;
-    // std::cout << "ObjectBush1: "
-    //           <<
-    //           static_cast<uint64_t>(Hash("ObjectBush1"))
-    //           << std::endl;
-    // std::cout << "ObjectBush2: "
-    //           <<
-    //           static_cast<uint64_t>(Hash("ObjectBush2"))
-    //           << std::endl;
-
     _<Engine>().Initialize(_<Theme0Properties>().GetGameWindowTitle(),
                            _<Theme0Properties>().GetClearColor());
 
