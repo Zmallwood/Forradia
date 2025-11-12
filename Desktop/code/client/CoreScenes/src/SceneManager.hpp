e5c9bef7b4f47d662c5a4eb5c5e509ad66bb9275
Andreas Åkerberg <zmallwood@proton.me>
2025-11-12 14:01:45 +0100

    Save

--- Desktop/code/client/CoreScenes/src/SceneManager.hpp
+++ Desktop/code/client/CoreScenes/src/SceneManager.hpp
@@ -10,19 +10,40 @@ namespace Forradia
 {
     class IScene;
 
+    ///
+    /// The manager for the scenes.
+    ///
     class SceneManager
     {
       public:
+        ///
+        /// Adds a scene to the manager.
+        ///
+        /// @param sceneName The name of the scene.
+        /// @param scene The scene to add.
+        ///
         void AddScene(StringView sceneName, IScene &scene);
 
+        ///
+        /// Goes to a scene.
+        ///
+        /// @param sceneName The name of the scene to go to.
+        ///
         void GoToScene(StringView sceneName);
 
+        ///
+        /// Updates the current scene.
+        ///
         void UpdateCurrentScene();
 
+        ///
+        /// Renders the current scene.
+        ///
         void RenderCurrentScene() const;
 
       private:
-        std::map<int, IScene &> m_scenes;
-        int m_currentScene{0};
+        std::map<int, IScene &> m_scenes; ///< The scenes.
+
+        int m_currentScene{0}; ///< The current scene, as the name hash of the scene.
     };
 }
