bd3ef0293140826d11b8cce5faa3f949c42b8c66
Andreas Åkerberg <zmallwood@proton.me>
2025-11-18 22:20:10 +0100

    Save

--- Desktop/code/client/CoreScenes/src/IScene.hpp
+++ Desktop/code/client/CoreScenes/src/IScene.hpp
@@ -18,49 +18,89 @@ namespace Forradia
     class IScene
     {
       public:
+        ///
+        /// Initializes the scene.
+        ///
         void Initialize();
 
-        void Update();
-
-        void Render() const;
-
+        ///
+        /// Called when the scene is entered.
+        ///
         void OnEnter();
 
+        ///
+        /// Updates the scene.
+        ///
+        void Update();
+
+        ///
+        /// Renders the scene.
+        ///
+        void Render() const;
+
+        ///
+        /// Sets the derived initialize function.
+        ///
+        /// @param value The function to set.
+        ///
         void SetInitializeDerived(Function<void()> value)
         {
             m_initializeDerived = value;
         }
 
+        ///
+        /// Sets the derived on enter function.
+        ///
+        /// @param value The function to set.
+        ///
         void SetOnEnterDerived(Function<void()> value)
         {
             m_onEnterDerived = value;
         }
 
+        ///
+        /// Sets the derived update function.
+        ///
+        /// @param value The function to set.
+        ///
         void SetUpdateDerived(Function<void()> value)
         {
             m_updateDerived = value;
         }
 
+        ///
+        /// Sets the derived render function.
+        ///
+        /// @param value The function to set.
+        ///
         void SetRenderDerived(Function<void()> value)
         {
             m_renderDerived = value;
         }
 
       protected:
+        ///
+        /// Gets the GUI.
+        ///
+        /// @return The GUI.
+        ///
         auto GetGUI() const
         {
             return m_gui;
         }
 
       private:
-        SharedPtr<GUI> m_gui;
+        SharedPtr<GUI> m_gui; ///< The GUI.
 
-        Function<void()> m_initializeDerived{[] {}};
+        /// TODO: Consider if the Functions should be replaced with virtual methods. Right now it is
+        /// used to make it work with Python bindings.
 
-        Function<void()> m_onEnterDerived{[] {}};
+        Function<void()> m_initializeDerived{[] {}}; ///< The derived initialize function.
 
-        Function<void()> m_updateDerived{[] {}};
+        Function<void()> m_onEnterDerived{[] {}}; ///< The derived on enter function.
 
-        Function<void()> m_renderDerived{[] {}};
+        Function<void()> m_updateDerived{[] {}}; ///< The derived update function.
+
+        Function<void()> m_renderDerived{[] {}}; ///< The derived render function.
     };
 }
