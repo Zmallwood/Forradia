e577aaccc873320469fb975b79e07a55bd9b4b7b
Andreas Åkerberg <zmallwood@proton.me>
2025-12-16 17:05:39 +0100

    Save

--- Desktop/code/client/Theme0WorldGenerationEntities/src/WorldGeneratorEntities.cpp
+++ Desktop/code/client/Theme0WorldGenerationEntities/src/WorldGeneratorEntities.cpp
@@ -16,149 +16,83 @@
 
 namespace Forradia::Theme0 {
     void WorldGeneratorEntities::GenerateEntities() const {
-        // Do the steps to generate entities.
-
         GenerateCreaturesInEcosystems();
-
         GenerateRobotsInAreas();
     }
 
     void WorldGeneratorEntities::GenerateRobotsInAreas() const {
-        // Obtain required data.
-
         auto worldArea{GetWorldArea()};
-
         auto worldAreaSize{worldArea->GetSize()};
-
         auto worldScaling{GetWorldScaling()};
 
         // Generate robots in clusters and patrol areas.
         // Robots avoid water but can be found in various terrain types.
 
-        // Number of robot clusters to generate.
-
         auto numRobotClusters{38 + GetRandomInt(7)};
 
         // Generate robots in clusters.
-
         for (auto cluster = 0; cluster < numRobotClusters; cluster++) {
-            // Generate a random center point for the cluster.
-
             auto centerX{GetRandomInt(worldAreaSize.width)};
-
             auto centerY{GetRandomInt(worldAreaSize.height)};
 
-            // Get the tile at the center point.
-
             auto centerTile = worldArea->GetTile(centerX, centerY);
-
-            // If the tile is invalid or is water.
 
             if (!centerTile || centerTile->GetGround() == Hash("GroundWater")) {
                 continue;
             }
 
-            // Generate a random radius for the cluster.
-
             auto clusterRadius{10 + GetRandomInt(15)};
-
-            // Number of robots to generate in the cluster.
-
             auto robotsInCluster{5 + GetRandomInt(10)};
 
             // Generate robots in the cluster.
-
             for (auto r = 0; r < robotsInCluster; r++) {
                 // Generate a random angle.
-
                 auto angle = GetRandomInt(360) * M_PI / 180.0f;
 
                 // Generate a random distance.
-
                 auto distance = GetRandomInt(clusterRadius);
 
-                // Calculate the coordinates of the robot.
-
                 auto robotX = centerX + CInt(std::cos(angle) * distance);
-
                 auto robotY = centerY + CInt(std::sin(angle) * distance);
 
-                // If the coordinates are out of bounds.
-
                 if (!worldArea->IsValidCoordinate(robotX, robotY)) {
-                    // Skip this robot.
-
                     continue;
                 }
-
-                // Get the tile at the coordinates.
 
                 auto robotTile = worldArea->GetTile(robotX, robotY);
 
-                // If the tile is invalid, or already has a robot or is water.
-
                 if (!robotTile || robotTile->GetRobot() ||
                     robotTile->GetGround() == Hash("GroundWater")) {
-                    // Skip this robot.
-
                     continue;
                 }
 
-                // Create a new robot.
-
                 auto newRobot{std::make_shared<Theme0::Robot>("RobotMechWolf", robotX, robotY)};
 
-                // Set the robot on the tile.
-
                 robotTile->SetRobot(newRobot);
-
-                // Add the robot to the world area robots mirror.
 
                 worldArea->GetRobotsMirrorRef().insert({robotTile->GetRobot(), {robotX, robotY}});
             }
         }
 
         // Also add scattered robots throughout the world.
-
-        // Number of scattered robots to generate.
-
         auto numScatteredRobots{220 * worldScaling + GetRandomInt(40 * worldScaling)};
 
-        // Generate scattered robots.
-
         for (auto i = 0; i < numScatteredRobots; i++) {
-            // Generate random coordinates.
-
             auto x{GetRandomInt(worldAreaSize.width)};
-
             auto y{GetRandomInt(worldAreaSize.height)};
-
-            // Get the tile at the coordinates.
 
             auto tile{worldArea->GetTile(x, y)};
 
-            // If the tile is invalid, or already has a robot or is water.
-
             if (!tile || tile->GetRobot() || tile->GetGround() == Hash("GroundWater")) {
-                // Skip this robot.
-
                 continue;
             }
 
             // Robots can appear anywhere except water, with equal probability.
 
-            // If a random number is less than 15.
-
             if (GetRandomInt(100) < 15) {
-                // Create a new robot.
-
                 auto newRobot{std::make_shared<Theme0::Robot>("RobotMechWolf", x, y)};
 
-                // Set the robot on the tile.
-
                 tile->SetRobot(newRobot);
-
-                // Add the robot to the world area robots mirror.
 
                 worldArea->GetRobotsMirrorRef().insert({tile->GetRobot(), {x, y}});
             }
@@ -166,30 +100,16 @@ namespace Forradia::Theme0 {
     }
 
     bool WorldGeneratorEntities::IsNearWater(int x, int y, int radius) const {
-        // Check all tiles in the radius.
-
         for (auto checkY = y - radius; checkY <= y + radius; checkY++) {
             for (auto checkX = x - radius; checkX <= x + radius; checkX++) {
-                // If the coordinates are out of bounds.
-
                 if (!GetWorldArea()->IsValidCoordinate(checkX, checkY)) {
-                    // Skip this tile.
-
                     continue;
                 }
 
-                // Get the tile at the given coordinates.
-
                 auto tile{GetWorldArea()->GetTile(checkX, checkY)};
 
-                // Check if the tile is water.
-
                 if (tile && tile->GetGround() == Hash("GroundWater")) {
-                    // Calculate the distance between the tile and the given coordinates.
-
                     auto distance{GetDistance(x, y, checkX, checkY)};
-
-                    // Return true if the distance is less than or equal to the radius.
 
                     if (distance <= radius) {
                         return true;
@@ -197,8 +117,6 @@ namespace Forradia::Theme0 {
                 }
             }
         }
-
-        // Return false if no adjacent tile is water.
 
         return false;
     }
--- Desktop/code/client/Theme0WorldGenerationEntities/src/WorldGeneratorEntities_GenerateCreaturesInEcosystems.cpp
+++ Desktop/code/client/Theme0WorldGenerationEntities/src/WorldGeneratorEntities_GenerateCreaturesInEcosystems.cpp
@@ -16,149 +16,77 @@
 
 namespace Forradia::Theme0 {
     void WorldGeneratorEntities::GenerateCreaturesInEcosystems() const {
-        // Obtain required data.
-
         auto worldArea{GetWorldArea()};
-
         auto worldAreaSize{worldArea->GetSize()};
-
         auto worldScaling{GetWorldScaling()};
 
         // Generate white rabbits - prefer areas near water and in meadows/grass.
-
-        // Number of white rabbits to generate.
-
         auto numWhiteRabbits{180 * worldScaling + GetRandomInt(40 * worldScaling)};
 
-        // Generate white rabbits.
-
         for (auto i = 0; i < numWhiteRabbits; i++) {
-            // Generate a random position.
-
             auto x{GetRandomInt(worldAreaSize.width)};
-
             auto y{GetRandomInt(worldAreaSize.height)};
-
-            // Get the tile at the position.
 
             auto tile{worldArea->GetTile(x, y)};
 
-            // If the tile is invalid, or already has a creature or is water.
-
             if (!tile || tile->GetCreature() || tile->GetGround() == Hash("GroundWater")) {
-                // Skip this position.
-
                 continue;
             }
 
-            // To hold result from the algorithm for determining if the rabbit should be placed
-            // at this position.
-
             auto prefersLocation{false};
 
-            // Rabbits prefer grass areas.
-
             if (tile->GetGround() == Hash("GroundGrass")) {
-                // Check if there is water within 8 tiles of the rabbit.
-
                 if (IsNearWater(x, y, 8)) {
-                    // Higher chance if near water.
-
                     prefersLocation = GetRandomInt(100) < 40;
                 } else {
-                    // Lower chance if not near water.
-
                     prefersLocation = GetRandomInt(100) < 20;
                 }
             } else if (tile->GetGround() == Hash("GroundDirt")) {
-                // Lower chance on dirt.
-
                 prefersLocation = GetRandomInt(100) < 5;
             }
 
-            // If the rabbit should be placed at this position.
-
             if (prefersLocation) {
-                // Create a new rabbit.
-
                 auto newCreature{std::make_shared<Theme0::Creature>("CreatureWhiteRabbit")};
 
-                // Set the rabbit on the tile.
-
                 tile->SetCreature(newCreature);
-
-                // Add the rabbit to the world area creatures mirror.
 
                 worldArea->GetCreaturesMirrorRef().insert({tile->GetCreature(), {x, y}});
             }
         }
 
         // Generate red birds - prefer areas with trees (forests) but also allow in open areas.
-
-        // Number of red birds to generate.
-
         auto numRedBirds{120 * worldScaling + GetRandomInt(30 * worldScaling)};
 
-        // Generate the red birds.
-
         for (auto i = 0; i < numRedBirds; i++) {
-            // Generate a random position.
-
             auto x{GetRandomInt(worldAreaSize.width)};
-
             auto y{GetRandomInt(worldAreaSize.height)};
-
-            // Get the tile at the position.
 
             auto tile{worldArea->GetTile(x, y)};
 
-            // If the tile is invalid, or already has a creature or is water.
-
             if (!tile || tile->GetCreature() || tile->GetGround() == Hash("GroundWater")) {
-                // Skip this position.
-
                 continue;
             }
 
             // Birds prefer areas with trees nearby (forests).
             // Check for trees in surrounding area.
 
-            // To hold the number of trees nearby.
-
             auto nearbyTreesCount{0};
 
             // Check for trees in the surrounding area.
-
             for (auto checkY = y - 3; checkY <= y + 3; checkY++) {
                 for (auto checkX = x - 3; checkX <= x + 3; checkX++) {
-                    // If the current tile is the same as the bird's tile.
-
                     if (checkX == x && checkY == y) {
-                        // Skip the current tile.
-
                         continue;
                     }
-
-                    // If the current tile is out of bounds.
 
                     if (!worldArea->IsValidCoordinate(checkX, checkY)) {
-                        // Skip the current tile.
-
                         continue;
                     }
-
-                    // Get the tile at the position.
 
                     auto nearbyTile{worldArea->GetTile(checkX, checkY)};
 
-                    // If the tile is valid.
-
                     if (nearbyTile) {
-                        // Get the objects stack from the tile.
-
                         auto objectsStack{nearbyTile->GetObjectsStack()};
-
-                        // If the objects stack is not empty.
 
                         if (objectsStack->GetSize() > 0) {
                             for (auto object : objectsStack->GetObjects()) {
@@ -167,10 +95,8 @@ namespace Forradia::Theme0 {
                                 if (objectType == Hash("ObjectFirTree") ||
                                     objectType == Hash("ObjectBirchTree")) {
                                     nearbyTreesCount++;
-
                                     // Birds can perch on trees, so nearby trees increase
                                     // probability.
-
                                     nearbyTreesCount++;
                                 }
                             }
@@ -179,171 +105,96 @@ namespace Forradia::Theme0 {
                 }
             }
 
-            // To hold result from the algorithm for determining if the bird should be placed at
-            // this position.
-
             auto prefersLocation{false};
-
-            // Birds strongly prefer areas with trees (forests).
 
             if (nearbyTreesCount >= 2) {
                 // High probability in forest areas.
-
                 prefersLocation = GetRandomInt(100) < 50;
             } else if (nearbyTreesCount == 1) {
                 // Moderate probability near a single tree.
-
                 prefersLocation = GetRandomInt(100) < 25;
             } else if (tile->GetGround() == Hash("GroundGrass")) {
                 // Lower probability in open grass areas (birds can still be found there).
-
                 prefersLocation = GetRandomInt(100) < 8;
             }
 
-            // If the bird should be placed at this position.
-
             if (prefersLocation) {
-                // Create a new bird.
-
                 auto newCreature{std::make_shared<Theme0::Creature>("CreatureRedBird")};
 
-                // Set the bird on the tile.
-
                 tile->SetCreature(newCreature);
-
-                // Add the bird to the world area creatures mirror.
 
                 worldArea->GetCreaturesMirrorRef().insert({tile->GetCreature(), {x, y}});
             }
         }
 
         // Create creature clusters near water sources (more realistic ecosystems).
-
         auto numWaterSources{15 + GetRandomInt(10)};
 
-        // Generate the creature clusters near water sources.
-
         for (auto source = 0; source < numWaterSources; source++) {
-            // Generate a random position.
-
             auto x{GetRandomInt(worldAreaSize.width)};
-
             auto y{GetRandomInt(worldAreaSize.height)};
-
-            // Get the tile at the position.
 
             auto tile{worldArea->GetTile(x, y)};
 
-            // To hold the number of attempts to find a water tile.
-
             auto attempts{0};
 
-            // To hold the x coordinate of the water tile.
-
             auto waterX{0};
-
-            // To hold the y coordinate of the water tile.
-
             auto waterY{0};
-
-            // To hold if a water tile was found.
 
             auto foundWater{false};
 
             // Find a water tile.
-
             while (attempts < 30 && !foundWater) {
-                // Generate a random position.
-
                 waterX = GetRandomInt(worldAreaSize.width);
-
                 waterY = GetRandomInt(worldAreaSize.height);
-
-                // Get the tile at the position.
 
                 auto tile{worldArea->GetTile(waterX, waterY)};
 
-                // If the tile is valid and is water.
-
                 if (tile && tile->GetGround() == Hash("GroundWater")) {
-                    // Water tile has been found.
-
                     foundWater = true;
                 }
-
-                // Increment the attempts counter.
 
                 attempts++;
             }
 
-            // If no water tile was found.
-
             if (!foundWater) {
-                // Skip this source.
-
                 continue;
             }
 
             // Create a small ecosystem around the water source.
 
-            // Determine the radius of the ecosystem.
-
             auto ecosystemRadius{5 + GetRandomInt(8)};
-
-            // Determine the number of creatures in the ecosystem.
 
             auto creaturesInEcosystem{3 + GetRandomInt(5)};
 
             // Generate the creatures in the ecosystem.
-
             for (auto c = 0; c < creaturesInEcosystem; c++) {
                 // Generate a random angle.
-
                 auto angle{GetRandomInt(360) * M_PI / 180.0f};
 
                 // Generate a random distance.
-
                 auto distance{2 + GetRandomInt(ecosystemRadius)};
 
-                // Calculate the coordinates of the creature.
-
                 auto creatureX{waterX + CInt(std::cos(angle) * distance)};
-
                 auto creatureY{waterY + CInt(std::sin(angle) * distance)};
 
                 // If the coordinates are out of bounds.
-
                 if (!worldArea->IsValidCoordinate(creatureX, creatureY)) {
-                    // Skip this creature.
-
                     continue;
                 }
 
-                // Get the tile at the coordinates.
-
                 auto creatureTile{worldArea->GetTile(creatureX, creatureY)};
-
-                // If the tile is invalid, or already has a creature or is water.
 
                 if (!creatureTile || creatureTile->GetCreature() ||
                     creatureTile->GetGround() == Hash("GroundWater")) {
-                    // Skip this creature.
-
                     continue;
                 }
 
                 // Prefer grass for the ecosystem.
-
                 if (creatureTile->GetGround() == Hash("GroundGrass") && GetRandomInt(100) < 60) {
-                    // Create a new creature.
-
                     auto newCreature{std::make_shared<Theme0::Creature>("CreatureWhiteRabbit")};
 
-                    // Set the creature on the tile.
-
                     creatureTile->SetCreature(newCreature);
-
-                    // Add the creature to the world area creatures mirror.
 
                     worldArea->GetCreaturesMirrorRef().insert(
                         {creatureTile->GetCreature(), {creatureX, creatureY}});
