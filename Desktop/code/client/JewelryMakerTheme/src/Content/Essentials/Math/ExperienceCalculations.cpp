6e064acfb67fc5878d25ba2125305c8c2f326856
Andreas Åkerberg <zmallwood@proton.me>
2025-12-26 09:58:01 +0100

    Save

--- Desktop/code/client/JewelryMakerTheme/src/Content/Essentials/Math/ExperienceCalculations.cpp
+++ Desktop/code/client/JewelryMakerTheme/src/Content/Essentials/Math/ExperienceCalculations.cpp
@@ -9,48 +9,54 @@
 
 namespace ForradiaEngine::JewelryMakerTheme
 {
-    auto calculateCurrentLevel(int experience) -> int
-    {
-        auto level{std::trunc(log2f(pow(experience + 70, 2) / 5000)) + 1};
+    /* Level calculation */ // clang-format off
+        auto calculateCurrentLevel(int experience) -> int
+        {
+            auto level{std::trunc(log2f(pow(experience + 70, 2) / 5000)) + 1};
 
-        return static_cast<int>(level);
-    }
+            return static_cast<int>(level);
+        }
+    // clang-format on
 
-    auto calculateExperienceForLevel(int level) -> int
-    {
-        auto experience{std::max(sqrtf(powf(2, level - 1) * 5000) - 70, 0.0F)};
+    /* Experience calculations */ // clang-format off
+        auto calculateExperienceForLevel(int level) -> int
+        {
+            auto experience{std::max(sqrtf(powf(2, level - 1) * 5000) - 70, 0.0F)};
 
-        return static_cast<int>(experience);
-    }
+            return static_cast<int>(experience);
+        }
 
-    auto calculateExperienceDifferenceToNextLevel(int experience) -> int
-    {
-        auto currentLevel{calculateCurrentLevel(experience)};
-        auto nextLevel{currentLevel + 1};
-        auto totalExperienceForCurrentLevel{calculateExperienceForLevel(currentLevel)};
-        auto totalExperienceForNextLevel{calculateExperienceForLevel(nextLevel)};
+        auto calculateExperienceDifferenceToNextLevel(int experience) -> int
+        {
+            auto currentLevel{calculateCurrentLevel(experience)};
+            auto nextLevel{currentLevel + 1};
+            auto totalExperienceForCurrentLevel{calculateExperienceForLevel(currentLevel)};
+            auto totalExperienceForNextLevel{calculateExperienceForLevel(nextLevel)};
 
-        return totalExperienceForNextLevel - totalExperienceForCurrentLevel;
-    }
+            return totalExperienceForNextLevel - totalExperienceForCurrentLevel;
+        }
 
-    auto calculateExperienceRequiredForCurrentLevelStart(int experience) -> int
-    {
-        auto currentLevel{calculateCurrentLevel(experience)};
+        auto calculateExperienceRequiredForCurrentLevelStart(int experience) -> int
+        {
+            auto currentLevel{calculateCurrentLevel(experience)};
 
-        return calculateExperienceForLevel(currentLevel);
-    }
+            return calculateExperienceForLevel(currentLevel);
+        }
 
-    auto calculateExperienceGainedSinceLevelStart(int experience) -> int
-    {
-        return experience - calculateExperienceRequiredForCurrentLevelStart(experience);
-    }
+        auto calculateExperienceGainedSinceLevelStart(int experience) -> int
+        {
+            return experience - calculateExperienceRequiredForCurrentLevelStart(experience);
+        }
+    // clang-format on
 
-    auto calculateFractionalExperienceProgress(int experience) -> float
-    {
-        auto experienceGain{experience -
-                            calculateExperienceRequiredForCurrentLevelStart(experience)};
-        auto experienceDiff{calculateExperienceDifferenceToNextLevel(experience)};
+    /* Fractional experience calculation */ // clang-format off
+        auto calculateFractionalExperienceProgress(int experience) -> float
+        {
+            auto experienceGain{experience -
+                                calculateExperienceRequiredForCurrentLevelStart(experience)};
+            auto experienceDiff{calculateExperienceDifferenceToNextLevel(experience)};
 
-        return static_cast<float>(experienceGain) / experienceDiff;
-    }
+            return static_cast<float>(experienceGain) / experienceDiff;
+        }
+    // clang-format on
 }
--- Desktop/code/client/JewelryMakerTheme/src/Content/Essentials/Math/ExperienceCalculations.hpp
+++ Desktop/code/client/JewelryMakerTheme/src/Content/Essentials/Math/ExperienceCalculations.hpp
@@ -7,51 +7,57 @@
 
 namespace ForradiaEngine::JewelryMakerTheme
 {
-    /**
-     *  Calculates the current level based on the experience.
-     *
-     *  @param experience The experience.
-     *  @return The current level.
-     */
-    auto calculateCurrentLevel(int experience) -> int;
+    /* Level calculation */ // clang-format off
+        /**
+        *  Calculates the current level based on the experience.
+        *
+        *  @param experience The experience.
+        *  @return The current level.
+        */
+        auto calculateCurrentLevel(int experience) -> int;
+    // clang-format on
 
-    /**
-     *  Calculates the experience required for the given level.
-     *
-     *  @param level The level.
-     *  @return The experience required for the given level.
-     */
-    auto calculateExperienceForLevel(int level) -> int;
+    /* Experience calculations */ // clang-format off
+        /**
+        *  Calculates the experience required for the given level.
+        *
+        *  @param level The level.
+        *  @return The experience required for the given level.
+        */
+        auto calculateExperienceForLevel(int level) -> int;
 
-    /**
-     *  Calculates the experience difference to the next level.
-     *
-     *  @param experience The experience.
-     *  @return The experience difference to the next level.
-     */
-    auto calculateExperienceDifferenceToNextLevel(int experience) -> int;
+        /**
+        *  Calculates the experience difference to the next level.
+        *
+        *  @param experience The experience.
+        *  @return The experience difference to the next level.
+        */
+        auto calculateExperienceDifferenceToNextLevel(int experience) -> int;
 
-    /**
-     *  Calculates the experience required for the current level start.
-     *
-     *  @param experience The experience.
-     *  @return The experience required for the current level start.
-     */
-    auto calculateExperienceRequiredForCurrentLevelStart(int experience) -> int;
+        /**
+        *  Calculates the experience required for the current level start.
+        *
+        *  @param experience The experience.
+        *  @return The experience required for the current level start.
+        */
+        auto calculateExperienceRequiredForCurrentLevelStart(int experience) -> int;
 
-    /**
-     *  Calculates the experience gained since the level start.
-     *
-     *  @param experience The experience.
-     *  @return The experience gained since the level start.
-     */
-    auto calculateExperienceGainedSinceLevelStart(int experience) -> int;
+        /**
+        *  Calculates the experience gained since the level start.
+        *
+        *  @param experience The experience.
+        *  @return The experience gained since the level start.
+        */
+        auto calculateExperienceGainedSinceLevelStart(int experience) -> int;
+    // clang-format on
 
-    /**
-     *  Calculates the fractional experience progress.
-     *
-     *  @param experience The experience.
-     *  @return The fractional experience progress.
-     */
-    auto calculateFractionalExperienceProgress(int experience) -> float;
+    /* Fractional experience calculation */ // clang-format off
+        /**
+        *  Calculates the fractional experience progress.
+        *
+        *  @param experience The experience.
+        *  @return The fractional experience progress.
+        */
+        auto calculateFractionalExperienceProgress(int experience) -> float;
+    // clang-format on
 }
