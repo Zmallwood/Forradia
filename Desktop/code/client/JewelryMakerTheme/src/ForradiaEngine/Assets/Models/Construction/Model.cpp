39febd3f0c58a03dc65c699fa4ad4e4483d5e082
Andreas Åkerberg <zmallwood@proton.me>
2025-12-27 10:08:06 +0100

    Save

--- Desktop/code/client/JewelryMakerTheme/src/ForradiaEngine/Assets/Models/Construction/Model.cpp
+++ Desktop/code/client/JewelryMakerTheme/src/ForradiaEngine/Assets/Models/Construction/Model.cpp
@@ -15,23 +15,180 @@
 
 namespace ForradiaEngine
 {
-    auto Model::initialize(std::string_view filePath) -> void
-    {
-        Assimp::Importer importer;
-
-        unsigned int flags{aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace |
-                           aiProcess_GenBoundingBoxes | aiProcess_FixInfacingNormals};
-
-        // Read the model file.
-        const aiScene *scene{importer.ReadFile(filePath.data(), flags)};
-
-        if (nullptr == scene || nullptr == scene->mRootNode)
+    /* Initialization */ // clang-format off
+        auto Model::initialize(std::string_view filePath) -> void
         {
-            throwError("Assimp could not load model: " + std::string(importer.GetErrorString()));
+            Assimp::Importer importer;
+
+            unsigned int flags{aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace |
+                            aiProcess_GenBoundingBoxes | aiProcess_FixInfacingNormals};
+
+            // Read the model file.
+            const aiScene *scene{importer.ReadFile(filePath.data(), flags)};
+
+            if (nullptr == scene || nullptr == scene->mRootNode)
+            {
+                throwError("Assimp could not load model: " + std::string(importer.GetErrorString()));
+            }
+            else
+            {
+                this->processNode(scene->mRootNode, scene, aiMatrix4x4());
+            }
         }
-        else
+    // clang-format on
+
+    /* Processing */ // clang-format off
+        auto Model::processNode(aiNode *node, const aiScene *scene, aiMatrix4x4 transformation) -> void
         {
-            this->processNode(scene->mRootNode, scene, aiMatrix4x4());
+            // Iterate over all meshes in the node.
+            for (unsigned int i = 0; i < node->mNumMeshes; i++)
+            {
+                // Access the mesh.
+                auto *mesh{scene->mMeshes[node->mMeshes[i]]};
+
+                // Multiply the transformation with the node's transformation.
+                transformation *= node->mTransformation;
+
+                // Process the mesh.
+                m_meshes.push_back(processMesh(mesh, scene, transformation));
+            }
+
+            // Iterate over all child nodes.
+            for (unsigned int i = 0; i < node->mNumChildren; i++)
+            {
+                // Process the child node (recursively).
+                this->processNode(node->mChildren[i], scene, transformation);
+            }
         }
-    }
+
+        auto Model::processMesh(aiMesh *mesh, const aiScene *scene, aiMatrix4x4 transformation)
+            -> GLMMesh
+        {
+            if (mesh == nullptr)
+            {
+                throwError("mesh is nullptr");
+            }
+
+            if (scene == nullptr)
+            {
+                throwError("scene is nullptr");
+            }
+
+            auto vertices{Model::getVertices(mesh, transformation)};
+            auto indices{Model::getIndices(mesh)};
+            auto textures{Model::getTextures(mesh, scene)};
+
+            return {vertices, indices, textures};
+        }
+    // clang-format on
+
+    /* Data retrieval */ // clang-format off
+        auto Model::getVertices(aiMesh *mesh, aiMatrix4x4 transformation) -> std::vector<GLMVertex>
+        {
+            if (mesh == nullptr)
+            {
+                throwError("mesh is nullptr");
+            }
+
+            std::vector<GLMVertex> vertices;
+
+            // Iterate over all vertices.
+            for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+            {
+                GLMVertex vertex;
+                glm::vec3 position;
+
+                // Get the transformed vertex position.
+                auto verts{transformation * mesh->mVertices[i]};
+
+                // Set the position coordinates.
+                position.x = verts.x;
+                position.y = verts.y;
+                position.z = verts.z;
+
+                vertex.position = position;
+
+                // If there are normals.
+                if (mesh->mNormals != nullptr)
+                {
+                    glm::vec3 normal;
+
+                    // Set the normal coordinates.
+                    normal.x = mesh->mNormals[i].x;
+                    normal.y = mesh->mNormals[i].y;
+                    normal.z = mesh->mNormals[i].z;
+
+                    vertex.normal = normal;
+                }
+
+                // If there are texture coordinates.
+                if (mesh->mTextureCoords[0] != nullptr)
+                {
+                    glm::vec2 texCoords;
+
+                    // Set the texture coordinates.
+                    texCoords.x = mesh->mTextureCoords[0][i].x;
+                    texCoords.y = mesh->mTextureCoords[0][i].y;
+
+                    vertex.uv = texCoords;
+                }
+                else
+                {
+                    // In case there are no texture coordinates, set them to zero.
+                    vertex.uv = glm::vec2(0, 0);
+                }
+
+                vertices.push_back(vertex);
+            }
+            return vertices;
+        }
+
+        auto Model::getIndices(aiMesh *mesh) -> std::vector<unsigned int>
+        {
+            if (mesh == nullptr)
+            {
+                throwError("mesh is null");
+            }
+
+            std::vector<unsigned int> indices;
+
+            // Iterate over all faces.
+            for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+            {
+                // Access the face.
+                auto face{mesh->mFaces[i]};
+
+                // Iterate over all indices in the face.
+                for (unsigned int j = 0; j < face.mNumIndices; j++)
+                {
+                    indices.push_back(face.mIndices[j]);
+                }
+            }
+            return indices;
+        }
+
+        auto Model::getTextures(aiMesh *mesh, const aiScene *scene) -> std::vector<Texture>
+        {
+            if (mesh == nullptr)
+            {
+                throwError("mesh is nullptr");
+            }
+
+            if (scene == nullptr)
+            {
+                throwError("scene is nullptr");
+            }
+
+            std::vector<Texture> textures;
+            aiString textureFilePath;
+
+            // Get the texture path.
+            scene->mMaterials[mesh->mMaterialIndex]->GetTexture(aiTextureType_DIFFUSE, 0,
+                                                                &textureFilePath);
+            // Add the texture to the results.
+            textures.emplace_back(getFileNameNoExtension(textureFilePath.C_Str()));
+
+            return textures;
+        }
+    // clang-format on
 }
